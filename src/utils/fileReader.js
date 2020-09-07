cbf8231345885d2146b8233d77808d25102a7e41
stankevich_is <stankevich_is@astral.ru>
2020-09-07 17:33:41 +0300

    encoding fix

--- src/utils/fileReader.js
+++ src/utils/fileReader.js
@@ -7,8 +7,7 @@ export const readFile = (file, openViewer) => {
   cp1251Reader.onloadend = (e) => {
     if (e.target.result.toLowerCase().includes(`encoding="windows-1251"`))
       return openViewer(file.name, e.target.result);
-    if (e.target.result.toLowerCase().includes(`encoding="utf-8"`))
-      return utfReader.readAsText(file, "utf-8");
+    return utfReader.readAsText(file, "utf-8");
   };
   cp1251Reader.readAsText(file, "CP1251");
 };
