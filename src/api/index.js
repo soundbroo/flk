9b7bbc7876c103c2d652fb56568728cfd3734b68
stankevich_is <stankevich_is@astral.ru>
2020-09-08 14:05:18 +0300

    api

--- src/api/index.js
+++ src/api/index.js
@@ -4,22 +4,29 @@ import uuid from "react-uuid";
 class AxiosService {
   constructor() {
     this.instance = axios.create({
-      baseURL: "https://api-flk.cloud.astral-dev.ru/check",
+      baseURL: "https://api-flk.cloud.astral-dev.ru/",
       withCredentials: false,
+      headers: {
+        // "Content-Type": "application/json",
+      },
     });
 
     this.makeFormData = (files) => {
       const formData = new FormData();
-      files.forEach((file) =>
-        formData.append("file", JSON.stringify({ data: file, name: uuid() }))
-      );
+      const filesWithUuid = files.map((file) => {
+        return { name: uuid(), data: file };
+      });
+      console.log(filesWithUuid);
+      filesWithUuid.forEach((file) => {
+        formData.append("file", file);
+      });
       return formData;
     };
   }
 
   checkFiles = async (files) => {
     const data = this.makeFormData(files);
-    await this.instance.post("/", data);
+    await this.instance.post("check", data);
   };
 }
 
